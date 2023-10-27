89ed64cc9662a4f1b8bc22f656f7512de021f300
Barnaba Krupowicz <barnaba.krupowicz@gmail.com>
2023-10-27 17:04:59 +0200

    Random changes

--- source/main.py
+++ source/main.py
@@ -1,7 +1,23 @@
+
 from procedures.preprocess_files import preprocess_files, PreprocessConfig
 
-preprocess_files("/media/barny/SSD4/MasterThesis/Data/alphapose_skeletons/ntu_coco",
-                 "/media/barny/SSD4/MasterThesis/Data/ntu_coco.2.combined", PreprocessConfig())
-preprocess_files("/media/barny/SSD4/MasterThesis/Data/alphapose_skeletons/ntu_120_coco",
-                 "/media/barny/SSD4/MasterThesis/Data/ntu_120_coco.2.combined", PreprocessConfig())
+# preprocess_files("/media/barny/SSD4/MasterThesis/Data/alphapose_skeletons/ntu_coco",
+#                  "/media/barny/SSD4/MasterThesis/Data/ntu_coco.f1.combined", PreprocessConfig())
+# preprocess_files("/media/barny/SSD4/MasterThesis/Data/alphapose_skeletons/ntu_120_coco",
+#                  "/media/barny/SSD4/MasterThesis/Data/ntu_120_coco.f1.combined", PreprocessConfig())
 # testing_generation()
+
+if __name__ == "__main__":
+    import torch
+    from procedures.single_file_pose import single_file_pose
+
+    # single_file_pose(
+    #     "/media/barny/SSD4/MasterThesis/Data/ntu_sample/S001C001P001R001A010_rgb.avi",
+    #     torch.device("cuda"),
+    #     "coco17",
+    # )
+    single_file_pose(
+            "/media/barny/SSD4/MasterThesis/Data/concatenated.avi",
+            torch.device("cuda"),
+            "coco17",
+        )
