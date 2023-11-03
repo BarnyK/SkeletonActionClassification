359f6033b9c41ff4a1088becc9ee389a3eca623b
Barnaba Krupowicz <barnaba.krupowicz@gmail.com>
2023-11-03 13:17:56 +0100

    Removed legacy sampler

--- source/datasets/sampler.py
+++ source/datasets/sampler.py
@@ -1,29 +1,6 @@
 import numpy as np
 
 
-def legacy_sampler(data: np.ndarray, window_length: int, samples_per_window: int) -> np.ndarray:
-    assert window_length / samples_per_window % 1 == 0
-
-    _, T, V, C = data.shape
-    if T < window_length:
-        # data is shorter than window
-        # position the data
-        new_data = np.zeros((*data.shape[:-3], window_length, V, C), dtype=np.float32)
-        t = window_length - T
-        pos = np.random.randint(0, t)
-        new_data[..., pos:pos + T, :, :] = data
-        # Fill missing
-        new_data[..., :pos, :, :] = data[..., 0, np.newaxis, :, :]
-        new_data[..., pos + T:, :, :] = data[..., -1, np.newaxis, :, :]
-        data = new_data
-        T = window_length
-
-    start_indice = np.random.randint(0, T - window_length + 1)
-    end_indice = start_indice + window_length
-    jump = window_length // samples_per_window
-    return data[..., start_indice:end_indice:jump, :, :]
-
-
 class Sampler:
     def __init__(self, window_length: int, samples_per_window: int, test_mode: bool = False, clips: int = 1,
                  seed: int = 255):
--- source/datasets/test_sampler.py
+++ source/datasets/test_sampler.py
@@ -2,24 +2,7 @@ from unittest import TestCase
 
 import numpy as np
 
-from datasets.sampler import legacy_sampler, Sampler
-
-
-class Test(TestCase):
-    def test_sampler_shorter(self):
-        data = np.random.random((2, 55, 17, 2))
-        win = legacy_sampler(data, 64, 32)
-        assert win.shape[1] == 32
-
-    def test_sampler_longer(self):
-        data = np.random.random((2, 255, 17, 2))
-        win = legacy_sampler(data, 64, 16)
-        assert win.shape[1] == 16
-
-    def test_sampler_bad_params(self):
-        data = np.random.random((2, 255, 17, 2))
-        with self.assertRaises(AssertionError) as context:
-            win = legacy_sampler(data, 64, 31)
+from datasets.sampler import Sampler
 
 
 class TestSampler(TestCase):
